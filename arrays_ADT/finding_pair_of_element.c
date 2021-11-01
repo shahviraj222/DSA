6b4af3401aa978c67b0978fa8ade740f23751043
shahviraj22 <viraj22feb@gmail.com>
2021-11-01 09:51:17 +0530

    commit

--- arrays_ADT/finding_pair_of_element.c
+++ arrays_ADT/finding_pair_of_element.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+// time complexity = O(n^2)
 void pair(int *a, int key , int n)
 {
     for (int i = 0; i < n-1; i++)
@@ -15,10 +16,57 @@ void pair(int *a, int key , int n)
     }
     
 }
+// time complexity = O(n) , hash method or hash table
+void pair_hash(int *a, int key , int n)
+{
+    int h[513]={0};
+  
+   for (int i = 0; i < n-1; i++)
+    {
+        if(key-a[i]<0)
+        {
+            
+        }
+        else
+        {
+       if (h[key-a[i]]==1)
+       {
+           printf("%d,%d\n",(key-a[i]),a[i]);
+       }
+        }
+       h[a[i]]=1;                            //we also write h[a[i]]++;
+    }
+    printf("&&&%d",h[key-a[3]]); //if element in hash array assign 1 auto matically then problem is created
+}
+// finding pair of element which is sorted
+void pair_sorted(int *a , int n , int key)
+{
+    int i=0,j=9;
+    while (i<j)
+    {
+        if(a[i]+a[j]==key)
+        {
+            printf("(%d,%d)",a[i],a[j]);
+            j--;
+            i++;
+        }
+        else if (a[i]+a[j]>key)
+        {
+            j--;
+        }
+        else
+        {
+            i++;
+        }
+        
+    }
+    
+}
 int main()
 { 
-    int a[10]={9,10,5,52,512,4,210,6,56,40}; //     don't give duplicates 
-    int k=14,n=10;              // finding element after processing procesure on pair of element
-    pair(a,k,n);
+    int a[10]={9,10,5,52,512,4,210,6,56,40};          //     don't give duplicates 
+    int s[10]={1,5,7,8,9,10,12,32,45,50};
+    int k=14,n=10,h=512;                                   // finding element after processing procesure on pair of element
+    pair_sorted(a,n,k);
     return 0;
 }
