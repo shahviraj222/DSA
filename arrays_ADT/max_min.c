d18e7c0c01530c0a1547f16e3429d5607ea5867b
shahviraj22 <viraj22feb@gmail.com>
2021-11-01 10:48:13 +0530

    array adt done

--- arrays_ADT/max_min.c
+++ arrays_ADT/max_min.c
@@ -39,11 +39,34 @@ int min(struct arr a1)
     }
     return max;
 }
+//finding both element max and min in one scan
+int max_min(int *a)
+{
+    int min=a[0],max=a[0];
+    int i=0;
+    while (i<10)
+    {
+      if (a[i]<min)
+      {
+          min=a[i];
+
+      }
+      else if (a[i]>max)
+      {
+         max=a[i];
+      }
+        i++;
+    }
+    printf("max=%d min=%d",max,min);
+    
+}
 int main()
 {
     struct arr a1 = {{0, 5, 6, 1, 51, 20, 54, 84, 50,0 }, 10, 10};
     int x;
+    int a[10]={0, 5, 6, 1, 51, 20, 154, 84, 50,0 };
     x=min(a1);
-    printf("Your maximum number in array is:%d",x);
+    printf("Your maximum number in array is:%d\n",x);
+    max_min(a);
     return 0;
 }
