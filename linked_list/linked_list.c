3004e66f8e5bf2b3484b2a3f9a1b26e525d9ce0c
shahviraj22 <viraj22feb@gmail.com>
2021-11-04 21:23:21 +0530

    starting with lined list

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -1,5 +1,38 @@
 #include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+void craet_node(struct node *temp,int a[])
+{
+    
+    for ( int i = 0; i <10 ; i++)
+    {
+        // printf("runing");
+        temp->next=(struct node *) malloc(sizeof(struct node));
+        temp->data=a[i];
+        temp=temp->next;
+    }
+    temp->next=NULL;
+    
+}
+void display(struct node *temp)
+{
+    while (temp->next != 0)
+    {
+          printf("%d ",temp->data);
+          temp=temp->next;
+    }
+    
+}
 int main()
 {
+    struct node *head;
+    head=(struct node *) malloc(sizeof(struct node));
+    int a[10]={10,20,2,6,262,5,1,15,205,45};
+    craet_node(head,a);
+    display(head);
     return 0;
 }
