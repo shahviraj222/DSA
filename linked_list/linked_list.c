5ebf4cdfe92704cfa42116b3ef69b7a462e7e93a
shahviraj22 <viraj22feb@gmail.com>
2021-11-06 11:19:20 +0530

    sum function created

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -29,19 +29,30 @@ void display(struct node *temp)
 }
 void diplay_recursive(struct node *temp)
 {
-    if(temp->next!=NULL)
+    if(temp->next != NULL)
     {
         printf("%d ",temp->data);
         diplay_recursive(temp->next);
     }
 }
+int sum(struct node *temp)
+{
+    int sum=0;
+    while (temp->next != NULL)
+    {
+        sum=sum+temp->data;
+        temp=temp->next;
+    }
+   return sum;
+}
 int main()
 {
     struct node *head;
     head=(struct node *) malloc(sizeof(struct node));
-    int a[10]={10,20,2,6,262,5,1,15,205,45};
+    int a[10]={10,20,2,6,0,5,1,15,205,45};
     craet_node(head,a);
     display(head);
     diplay_recursive(head);
+    printf("\nSum of all element is %d",sum(head));
     return 0;
 }
