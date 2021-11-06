8b9bf542b95d7c5fe923ef0b5addedd34d620900
shahviraj22 <viraj22feb@gmail.com>
2021-11-06 19:45:53 +0530

    when you get erro as sengmention and your code is right then check LOOP CONDITION

---
+++ linked_list/insert_element_sorted.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+void craet_node(struct node *temp,int a[])
+{
+    
+    for ( int i = 0; i <10 ; i++)
+    {
+        // printf("runing");
+        temp->next=(struct node *) malloc(sizeof(struct node));
+        temp->data=a[i];
+        temp=temp->next;
+    }
+    temp->next=NULL;                                //we have to assign the end of node 
+    
+}
+void display(struct node *temp)
+{
+    while (temp->next != 0)
+    {
+          printf("%d ",temp->data);
+          temp=temp->next;
+    }
+    
+}
+void insert(struct node *temp,int data)
+{
+    // struct node *temp=temp;
+    struct node *q;
+    struct node *w;
+    w=(struct node*) malloc(sizeof(struct node));
+    while (temp && data > temp->data)
+    {
+       q=temp;
+       temp=temp->next;
+    }
+    w->data=data;
+    w->next = q->next;
+    q->next = w;
+}
+
+
+int main()
+{
+    int a[10]={1,2,23,50,62,66,71,85,95,230};
+    int data=85;
+    struct node *head;
+    craet_node(head,a);
+    insert(head,120);
+    display(head);
+    return 0;
+}
