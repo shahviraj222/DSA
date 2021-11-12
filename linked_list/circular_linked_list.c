df9ac733101609765f137c04a7c328c62f313834
shahviraj22 <viraj22feb@gmail.com>
2021-11-12 19:58:49 +0530

    delete function is added

--- linked_list/circular_linked_list.c
+++ linked_list/circular_linked_list.c
@@ -4,8 +4,8 @@
 struct Node
 {
     int data;
-    struct Node *next;   //this statement become self referential structur 
-} *first = NULL;          //this is structure pointer created globally
+    struct Node *next; //this statement become self referential structur
+} *first = NULL;       //this is structure pointer created globally
 void create(int A[], int n)
 {
     int i;
@@ -19,56 +19,76 @@ void create(int A[], int n)
         t = (struct Node *)malloc(sizeof(struct Node));
         t->data = A[i];
         t->next = NULL;
-        last->next = t;                                //here my first ka next is changed
-        last = t;                                      
+        last->next = t; //here my first ka next is changed
+        last = t;
     }
-    t->next=first;
+    t->next = first;
 }
 void Display(struct Node *p)
 {
-    static int flag=0;
-    if (p!= first || flag==0)
+    static int flag = 0;
+    if (p != first || flag == 0)
     {
-        flag=1;
-        printf("%d ",p->data);
+        flag = 1;
+        printf("%d ", p->data);
         Display(p->next);
     }
 }
 //inserting element in circular linked list
-void insert(int position,struct Node* p,int data)
+void insert(int position, struct Node *p, int data)
 {
-    struct Node *t=(struct Node*) malloc(sizeof(struct Node));
-    t->data=data;
-    if (position==0)
+    struct Node *t = (struct Node *)malloc(sizeof(struct Node));
+    t->data = data;
+    if (position == 0)
     {
-        while (p->next!=first)
+        while (p->next != first)
         {
-            p=p->next;
-        }    
-       
-        t->next=first;       
-        p->next=t;           //this statement make my node as last node
+            p = p->next;
+        }
+
+        t->next = first;
+        p->next = t; //this statement make my node as last node
         // t->next=first;
-        // return t; 
+        // return t;
     }
     else
     {
-        for (int i = 0; i < position-1; i++)
+        for (int i = 0; i < position - 1; i++)
         {
-            p=p->next;
+            p = p->next;
         }
-        t->next=p->next;
-        p->next=t;
+        t->next = p->next;
+        p->next = t;
+    }
+}
+// DELETE MY NODE 
+// here my linked node start from 0th index 0 1 2 3 4 
+void delete (struct Node *p, int position)
+{
+    struct Node *q=p->next;    
+    if (position == 0)
+    {
         
     }
-    
+    else
+    {
+        for (int i = 0; i < position-2; i++)
+        {
+            p=p->next;
+            q=q->next; 
+        }
+        p->next=q->next;
+        free(q);
+    }
 }
 int main()
 {
     struct Node *temp;
     int A[] = {3, 5, 7, 10, 25, 8, 32, 2};
     create(A, 8);
-    insert(0,first,1000);       // postion mens after nth element 
+    // zero th position make my last node
+   //  insert(2, first, 1000); //index mens nth element position+1
+    delete(first,5);         //here nth position is deleted
     Display(first);
     return 0;
 }
