a81fc60f85cb795be81f52c127818dd3dabb664a
shahviraj22 <viraj22feb@gmail.com>
2021-11-12 19:34:28 +0530

    added new function to my program is INSERT

--- linked_list/circular_linked_list.c
+++ linked_list/circular_linked_list.c
@@ -34,11 +34,41 @@ void Display(struct Node *p)
         Display(p->next);
     }
 }
+//inserting element in circular linked list
+void insert(int position,struct Node* p,int data)
+{
+    struct Node *t=(struct Node*) malloc(sizeof(struct Node));
+    t->data=data;
+    if (position==0)
+    {
+        while (p->next!=first)
+        {
+            p=p->next;
+        }    
+       
+        t->next=first;       
+        p->next=t;           //this statement make my node as last node
+        // t->next=first;
+        // return t; 
+    }
+    else
+    {
+        for (int i = 0; i < position-1; i++)
+        {
+            p=p->next;
+        }
+        t->next=p->next;
+        p->next=t;
+        
+    }
+    
+}
 int main()
 {
     struct Node *temp;
     int A[] = {3, 5, 7, 10, 25, 8, 32, 2};
     create(A, 8);
+    insert(0,first,1000);       // postion mens after nth element 
     Display(first);
     return 0;
 }
