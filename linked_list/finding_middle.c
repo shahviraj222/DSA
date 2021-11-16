7b4b753dc1801750624b4f2a59f23eb00a74eab8
shahviraj22 <viraj22feb@gmail.com>
2021-11-16 12:16:10 +0530

    ok

--- linked_list/finding_middle.c
+++ linked_list/finding_middle.c
@@ -31,6 +31,11 @@ void Display(struct Node *p)
         p = p->next;
     }
 }
+//finding middle element there are three way 
+//1]finding length of list and n/2 print that node using loop
+//2]using stack of all address of node we calculate total address and then pop of 1/2 of the element in this        methode we scan the list once and then scan address of stack
+//3]created below using two pointer one is dubble of other if first complete then other at 1/2 of list
+
 int find_middle(struct Node* p)
 {   struct Node *q=p;
     while (p!=NULL)
@@ -51,8 +56,8 @@ int find_middle(struct Node* p)
 }
 int main()
 {
-    int a[10]={10,1,12,5,1,12,2011,2,210};
-    create(a,8);
+    int a[10]={10,1,12,5,1,12,2011,2,210,50};
+    create(a,10);
     int b=find_middle(first);
     Display(first);
 }
