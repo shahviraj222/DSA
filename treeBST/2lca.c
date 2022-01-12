945222c741839712e7240999683f296e8d4bb8d9
shahviraj222 <80911833+shahviraj222@users.noreply.github.com>
2022-01-12 12:45:02 +0530

    Update 2lca.c
    balance function is added

--- treeBST/2lca.c
+++ treeBST/2lca.c
@@ -63,6 +63,41 @@ struct node *search(struct node *root, int key)
     }
     return NULL;
 }
+int isBalanced(struct node* root, int* height)
+{
+    /* lh --> Height of left subtree 
+     rh --> Height of right subtree */
+    int lh = 0, rh = 0;
+
+    /* l will be true if left subtree is balanced 
+    and r will be true if right subtree is balanced */
+    int l = 0, r = 0;
+
+    if (root == NULL) {
+        *height = 0;
+        return 1;
+    }
+
+    /* Get the heights of left and right subtrees in lh and rh 
+    And store the returned values in l and r */
+    l = isBalanced(root->left, &lh);
+    r = isBalanced(root->right, &rh);
+
+    /* Height of current node is max of heights of left and 
+     right subtrees plus 1*/
+    *height = (lh > rh ? lh : rh) + 1;
+
+    /* If difference between heights of left and right 
+     subtrees is more than 2 then this node is not balanced
+     so return 0 */
+    if (abs(lh - rh) >= 2)
+        return 0;
+
+    /* If this node is balanced and left and right subtrees 
+    are balanced then return true */
+    else
+        return l && r;
+}
 int main()
 {
     int n, t, n1, n2;
@@ -90,5 +125,15 @@ int main()
         p = lca(root, n1, n2);
         printf("LCA of %d and %d is %d", n1, n2, p->data);
     }
+    int h,r;
+    r=isBalanced(root,&h);
+    if (r)
+    {
+        printf("\nTree is Balanced");
+    }
+    else
+    {
+        printf("\nTree is not Balanced");
+    } 
     return 0;
 }
